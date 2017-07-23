bc1f71d3f94d7e7467b44babd0aac2d7c1ed51d4
Alex Liu <alex@alexs-macbook-air.local>
2017-07-23 10:21:55 -0700

    1st Upload

---
+++ 3D Mobile App/3D Mobile AppTests/_D_Mobile_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  _D_Mobile_AppTests.swift
+//  3D Mobile AppTests
+//
+//  Created by Alex Liu on 7/23/17.
+//  Copyright © 2017 Alex Liu. All rights reserved.
+//
+
+import XCTest
+@testable import _D_Mobile_App
+
+class _D_Mobile_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
