7418a8e647efcf0c35346f688b980a9569ce4607
alexliuwet72 <30390986+alexliuwet72@users.noreply.github.com>
2017-08-13 09:53:33 -0700

    added new files pod into the folder of 3D Mobile

---
+++ 3D Mobile App/Pods/Target Support Files/Pods-3D Mobile AppTests/Pods-3D Mobile AppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_3D_Mobile_AppTests : NSObject
+@end
+@implementation PodsDummy_Pods_3D_Mobile_AppTests
+@end
---
+++ 3D Mobile App/Pods/Target Support Files/Pods-3D Mobile AppUITests/Pods-3D Mobile AppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_3D_Mobile_AppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_3D_Mobile_AppUITests
+@end
